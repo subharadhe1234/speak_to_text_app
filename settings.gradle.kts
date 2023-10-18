edf04afe65ed919aaa36bec295f283ec3c510a08
subharadhe1234 <subhadippratihar09@gmail.com>
2023-10-19 01:26:27 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "speak_to_text_app"
+include(":app")
